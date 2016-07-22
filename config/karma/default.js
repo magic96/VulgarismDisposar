7fa96b636eba34ce07a2f4c058ac972af4c731a6
qrdupel1996 <qrdupel1996@gmail.com>
2016-07-21 20:04:05 -0400

    Added directories with configuration.

---
+++ config/karma/default.js
@@ -0,0 +1,88 @@
+// Karma configuration
+// Generated on Thu Jul 21 2016 15:34:23 GMT-0400 (EDT)
+
+const convertCJS = require('rollup-plugin-commonjs');
+const nodeResolve = require('rollup-plugin-node-resolve');
+const babel = require('rollup-plugin-babel');
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '../../',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['mocha', 'chai-as-promised', 'chai'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      'node_modules/babel-polyfill/dist/polyfill.js',
+      'test/**/*.js'
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+      'test/**/*.js': [ 'rollup', 'coverage' ],
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress', 'coverage'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['PhantomJS'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: 1,
+    
+    rollupPreprocessor: {
+      rollup: {
+        plugins: [
+          nodeResolve(),
+          convertCJS(),
+          babel({ presets: ['es2015-rollup'] })
+        ]
+      },
+      bundle: { format: 'iife' }
+    },
+    
+    coverageReporter: { type: 'text' }
+  })
+}
---
+++ config/rollup/default.js
@@ -0,0 +1,15 @@
+import babel from 'rollup-plugin-babel';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import convertCJS from 'rollup-plugin-commonjs';
+
+export default {
+  format: 'umd',
+  entry: 'src/index.js',
+  dest: 'dist/index.umd.js',
+  moduleName: 'test',
+  plugins: [
+    nodeResolve(),
+    convertCJS(),
+    babel({ presets: ['es2015-rollup'] })
+  ],
+}
